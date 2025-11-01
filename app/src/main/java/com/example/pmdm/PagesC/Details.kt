b1cb182a9ee454625f90b5a031ff8b6d56c9ac46
RICARDO <RICARDO03-03-02@HOTMAIL.com>
2025-11-01 23:03:51 +0100

    Correciones en DetailsPage

--- app/src/main/java/com/example/pmdm/PagesC/Details.kt
+++ app/src/main/java/com/example/pmdm/PagesC/Details.kt
@@ -1,6 +1,7 @@
 package com.example.pmdm.PagesC
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -60,11 +62,11 @@ fun DetailsPage() {
                             .size(250.dp),
                         contentScale = ContentScale.Fit,
                     )
+                    Spacer(modifier = Modifier.height(30.dp))
                 }
 
                 items(infos) { config ->
                     TextBlockComponent(info = listOf(config))
-                    Spacer(modifier = Modifier.height(15.dp))
                 }
                 item {
                     Spacer(modifier = Modifier.height(80.dp))
