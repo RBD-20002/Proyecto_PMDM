3902e82c1aa7904360e7fc0105c28bd624d32997
RICARDO <RICARDO03-03-02@HOTMAIL.com>
2025-12-18 02:17:22 +0100

    limpieza de lineas innecesarias

--- app/src/main/java/com/example/pmdm/Components/FavColumnDisplay.kt
+++ app/src/main/java/com/example/pmdm/Components/FavColumnDisplay.kt
@@ -8,18 +8,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-
 import com.example.pmdm.RicardoComponent.VerticalAnimeCard
 import com.example.pmdm.model.DataProvider
 
--- app/src/main/java/com/example/pmdm/PagesC/FavoritesPage.kt
+++ app/src/main/java/com/example/pmdm/PagesC/FavoritesPage.kt
@@ -15,15 +15,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.example.pmdm.Components.CardConfig
 import com.example.pmdm.R
 import com.example.pmdm.model.DataProvider
 import com.example.pmdm.Components.FavColumnDisplay
-
 
 @Composable
 fun FavoritePage(navController: NavController) {
